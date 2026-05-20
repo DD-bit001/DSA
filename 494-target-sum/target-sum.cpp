ac4cff96276233159258755302e5839e15304d9b
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-05-20 16:04:04 +0530

    Time: 8 ms (60.28%) | Memory: 15.7 MB (39.59%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,36 @@
 class Solution {
-    int fun(vector<int>& nums,int ind,int target){
-        if (ind==0){
-            int cnt=0;
-            if(target-nums[ind]==0) {
-                cnt++;
-            }
-            if(target+nums[ind]==0) cnt++;
-            return cnt; 
+private:
+    int countSubsets(vector<int>& nums, int ind, int sum, vector<vector<int>>& dp) {
+        if (ind == 0) {
+            if (sum == 0 && nums[0] == 0) return 2;
+            if (sum == 0 || sum == nums[0]) return 1;
+            return 0;
         }
-        int add=fun(nums,ind-1,target+nums[ind]);
-        int sub=fun(nums,ind-1,target-nums[ind]);
-        return add+sub;
 
+        if (dp[ind][sum] != -1) return dp[ind][sum];
+
+        int notTake = countSubsets(nums, ind - 1, sum, dp);
+
+        int take = 0;
+        if (nums[ind] <= sum) {
+            take = countSubsets(nums, ind - 1, sum - nums[ind], dp);
+        }
+
+        return dp[ind][sum] = take + notTake;
     }
+
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-    return fun(nums,n-1,target);
+        int total = 0;
+        for (int x : nums) total += x;
 
+        if (total + target < 0) return 0;
+        if ((total + target) % 2 != 0) return 0;
 
-        
+        int s1 = (total + target) / 2;
+
+        vector<vector<int>> dp(nums.size(), vector<int>(s1 + 1, -1));
+
+        return countSubsets(nums, nums.size() - 1, s1, dp);
     }
 };
