553e71ebd46360f9101d8c9e2088dba8fc417125
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-06-03 14:54:11 +0530

    Time: 17 ms (91.80%) | Memory: 9.6 MB (98.38%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,10 +1,10 @@
 class Solution {
     public:
     int minInsertions(string s) {
-         vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,0));
+         vector<int>prev(s.size()+1);
+         vector<int>curr(s.size()+1);
          for(int i=0;i<=s.size();i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
+        prev[i]=0;
          }
         string t=s;
         reverse(t.begin(),t.end());
@@ -12,19 +12,20 @@ class Solution {
        for(int j=1;j<=s.size();j++){
 
         if(s[i-1]==t[j-1]){
-              dp[i][j]= 1+dp[i-1][j-1];
+              curr[j]= 1+prev[j-1];
 
         }
         else{
-            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            curr[j]=max(prev[j],curr[j-1]);
         }
+   
+       }
+     prev=curr;
        }
 
-       }
 
 
-
-        return  s.size()-dp[s.size()][s.size()] ;
+        return  s.size()-prev[s.size()] ;
         
     }
 };
