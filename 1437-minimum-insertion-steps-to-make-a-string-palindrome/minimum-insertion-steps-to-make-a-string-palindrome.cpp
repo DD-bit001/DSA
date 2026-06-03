d4e43879df167c611eeae1191009c84dc8706e3c
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-06-03 14:50:00 +0530

    Time: 38 ms (27.07%) | Memory: 31.7 MB (15.62%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,30 @@
 class Solution {
-      private :
-    int solve(string &text1,string &text2, int i , int j,vector<vector<int>> &dp){
-        if(i==0||j==0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        if(text1[i-1]==text2[j-1]){
-             return dp[i][j]= 1+solve(text1,text2,i-1,j-1,dp);
-
-        }
-        return dp[i][j]= 0+max(solve(text1,text2,i,j-1,dp),solve(text1,text2,i-1,j,dp));
-    }
-public:
+    public:
     int minInsertions(string s) {
-         vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
+         vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,0));
+         for(int i=0;i<=s.size();i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+         }
         string t=s;
         reverse(t.begin(),t.end());
-        return  s.size()-solve(s,t,s.size(),t.size(),dp) ;
+       for(int i=1;i<=s.size();i++){
+       for(int j=1;j<=s.size();j++){
+
+        if(s[i-1]==t[j-1]){
+              dp[i][j]= 1+dp[i-1][j-1];
+
+        }
+        else{
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+       }
+
+       }
+
+
+
+        return  s.size()-dp[s.size()][s.size()] ;
         
     }
 };
