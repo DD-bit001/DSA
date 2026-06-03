a09513b016aa703ffe7e919431b045925cd03051
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-06-03 14:36:10 +0530

    Time: 69 ms (7.03%) | Memory: 31.5 MB (59.25%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+      private :
+    int solve(string &text1,string &text2, int i , int j,vector<vector<int>> &dp){
+        if(i==0||j==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(text1[i-1]==text2[j-1]){
+             return dp[i][j]= 1+solve(text1,text2,i-1,j-1,dp);
+
+        }
+        return dp[i][j]= 0+max(solve(text1,text2,i,j-1,dp),solve(text1,text2,i-1,j,dp));
+    }
+public:
+    int minInsertions(string s) {
+         vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
+        string t=s;
+        reverse(t.begin(),t.end());
+        return  s.size()-solve(s,t,s.size(),t.size(),dp) ;
+        
+    }
+};
