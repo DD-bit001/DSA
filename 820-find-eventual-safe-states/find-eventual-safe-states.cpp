f607e0c18b97e2bace757327d28efc495aa38471
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-07-24 22:01:13 +0530

    Time: 6 ms (83.21%) | Memory: 53 MB (66.02%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    private:
+    bool traverse(vector<vector<int>>& graph,int node , vector<int> &vis,vector<int> &pathVis,vector<int> & ans,vector<int> &check){
+    vis[node]=1;
+    pathVis[node]=1;
+    bool temp=true;
+    for(auto it : graph[node]){
+        if(!vis[it]){
+            if((traverse(graph,it,vis,pathVis,ans,check))==false){
+                temp=false;
+            }
+        }
+        else if(pathVis[it]){
+            temp=false;
+        }
+         else if(check[it]==0){   
+        temp=false;
+    }
+    }
+    if(temp){
+        check[node]=1;
+    }
+    pathVis[node]=0;
+return temp;
+    }
+
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int v=graph.size();
+        vector<int> ans ;
+             vector<int> pathVis(v, 0);
+     vector<int> check(v,0);
+        vector<int> vis(v,0 );
+        for(int i=0;i<v;i++){
+           if(!vis[i]){
+            traverse(graph,i,vis,pathVis,ans,check);
+   }
+           }
+for(int i=0;i<v;i++){
+    if(check[i]) ans.push_back(i);
+}
+
+        return ans ;
+
+    }
+};
