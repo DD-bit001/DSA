8ed95bedf034cb448d25ed2cc02cb5dc35b52a4d
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-08-03 20:03:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (47.06%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+      bool find(vector<int>& nums,int s1, int s2,int start, int end,int turn,  vector<vector<int>>&dp){
+  if(start>end){
+  return s1>s2;
+  }
+ 
+return true;
+      }
+public:
+    bool stoneGame(vector<int>& piles) {
+      return true;
+    }
+};
