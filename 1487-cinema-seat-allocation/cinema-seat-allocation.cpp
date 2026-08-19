521cc55809acf93b2230d15d4e2d7661b20e3f41
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-08-19 23:09:16 +0530

    Time: 18 ms (81.93%) | Memory: 60.1 MB (73.48%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, int> mp;
+
+        for (auto &seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+
+            mp[row] |= (1 << col);
+        }
+
+
+        long long ans = 2LL * (n - mp.size());
+
+        for (auto &[row, mask] : mp) {
+            bool left  = !(mask & (1 << 2)) &&
+                         !(mask & (1 << 3)) &&
+                         !(mask & (1 << 4)) &&
+                         !(mask & (1 << 5));
+
+            bool right = !(mask & (1 << 6)) &&
+                         !(mask & (1 << 7)) &&
+                         !(mask & (1 << 8)) &&
+                         !(mask & (1 << 9));
+
+            bool middle = !(mask & (1 << 4)) &&
+                          !(mask & (1 << 5)) &&
+                          !(mask & (1 << 6)) &&
+                          !(mask & (1 << 7));
+
+            if (left && right) {
+                ans += 2;
+            }
+            else if (left || middle || right) {
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+};
