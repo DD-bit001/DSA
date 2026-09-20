8e5d059ff65864e2718e2328031a18da53c79ed6
DD-bit001 <ahmad.dilnawaz.md@gmail.com>
2026-09-20 21:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (95.77%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+
+    int ans=0;
+    for(int i=0;i<s.size();i++){
+        ans+=int ((i+1)*(abs(s[i]-'z')+1));
+    }
+
+
+return ans;
+        
+    }
+};
